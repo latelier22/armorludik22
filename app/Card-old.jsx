1e6de058f7d61c4f9497ffd38d7eb2496e475efe
Cyrille <latelier.cyrille@gmail.com>
2024-03-13 06:32:38 +0100

    sans bouton card

--- app/Card-old.jsx
+++ app/Card-old.jsx
@@ -37,9 +37,7 @@ const Card = ({ index, card, buttonColor, children, syliusCard }) => {
           </p>
           {children}
           <div className="flex flex-col items-center ">
-
-            {/* MODIF */}
-            {/* <a href={card.link}>
+            <a href={card.link}>
               <button
                 type="button"
                 className={`${buttonColor}  items-center rounded-2xl px-6 m-5 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]`}
@@ -48,7 +46,7 @@ const Card = ({ index, card, buttonColor, children, syliusCard }) => {
               >
                 {card.button}
               </button>
-            </a> */}
+            </a>
             <MyModal
               index={index}
               card={card}
